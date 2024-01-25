a8805ee5e43360fdf40d03c3eb2037ef90d88951
Kaitlyn R McKnight <kmcknig2@uwyo.edu>
2024-01-25 09:10:18 -0700

    updated figure 2 for formatting

--- data_analyses/F2_biomass.R
+++ data_analyses/F2_biomass.R
@@ -51,11 +51,11 @@ biomasstopfunc<-topbyfunc_subset%>% ggplot(aes(x=year, y=meanbiomass, group=spec
   facet_grid(vars(ntrt),vars(exp), labeller = labeller(ntrt = ntrt.labs, exp = exp.labs))+
   ylab(expression(paste("Mean aboveground biomass (g / ", m^2,")")))+
   xlab("")+
-  scale_colour_manual(values = c("#F8766D", "#CD9600", "#7CAE00", "#00BE67", "#00BFC4","#00A9FF","#C77CFF","#FF61CC", "black"), labels=c('Agr. rep. (C3 grass)', 'Art. lud. (Pe. forb)','Lath. ven. (Legume)', 'Poa pra. (C3 grass)','Poly. conv. (An. forb)','Rub. sp. (Shrub)', 'Schiz. scop. (C4 grass)', 'Soli. rig. (Pe. forb)', 'Total Biomass'))+
-  annotate("rect", xmin = 1982, xmax = 1988, ymin = 0, ymax = 550,alpha = .1,fill = "blue")+
-  annotate("rect", xmin = 1994, xmax = 2004, ymin = 0, ymax = 550,alpha = .1,fill = "blue")+
-  annotate(geom="text", x=1985, y=500, label="Transient \n phase", size=4)+
-  annotate(geom="text", x=1999, y=500, label="Post-transient \n phase", size=4)+
+  scale_colour_manual(values = c("cornflowerblue", "springgreen", "goldenrod1", "darkblue", "darkred","darkorchid","darkorange","darkgreen", "black"), labels=c('Agr. rep. (C3 grass)', 'Art. lud. (Pe. forb)','Lath. ven. (Legume)', 'Poa pra. (C3 grass)','Poly. conv. (An. forb)','Rub. sp. (Shrub)', 'Schiz. scop. (C4 grass)', 'Soli. rig. (Pe. forb)', 'Total Biomass'))+
+  annotate("rect", xmin = 1982, xmax = 1988, ymin = 0, ymax = 550,alpha = .1,fill = "grey60")+
+  annotate("rect", xmin = 1994, xmax = 2004, ymin = 0, ymax = 550,alpha = .1,fill = "grey60")+
+  annotate(geom="text", x=1985, y=500, label="Transient", size=4)+
+  annotate(geom="text", x=1999, y=500, label="Post-transient", size=4)+
   theme_bw()+
   theme(axis.text.x = element_text(color = "grey20", size = 14, angle = 0, hjust = .5, face = "plain"),
         axis.text.y = element_text(color = "grey20", size = 14, angle = 0, hjust = .5, vjust = 0, face = "plain"),
